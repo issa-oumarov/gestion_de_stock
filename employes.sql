d38b86c3ca6ad653a7cfc9a6572069a6737a0f21
issa-oumarov <130753664+issa-oumarov@users.noreply.github.com>
2023-05-09 11:36:55 +0200

    Add files via upload

---
+++ employes.sql
@@ -0,0 +1,76 @@
+-- MySQL dump 10.16  Distrib 10.1.48-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: db
+-- ------------------------------------------------------
+-- Server version	10.1.48-MariaDB-0+deb9u2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `employes`
+--
+
+DROP TABLE IF EXISTS `employes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `employes` (
+  `id` tinyint(4) DEFAULT NULL,
+  `nom` varchar(5) DEFAULT NULL,
+  `prenom` varchar(10) DEFAULT NULL,
+  `salaire` mediumint(9) DEFAULT NULL,
+  `id_service` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `employes`
+--
+
+LOCK TABLES `employes` WRITE;
+/*!40000 ALTER TABLE `employes` DISABLE KEYS */;
+INSERT INTO `employes` VALUES (1,'La','Plateforme',2220,7),(2,'La','Plateforme',2220,7),(3,'aaaa','bbbb',111,8),(4,'aaaa','bbbb',111,8),(5,'aaa','qqq',2222,22),(6,'aaaa','aaaaa',22121,22),(7,'aaaaa','aazaz',2121,21),(8,'AAaa','aaaaa',2222,11),(9,'AAaa','aaaaa',2222,11),(10,'AAA','AAAA',2222,22),(11,'AAA','AAAA',2222,22);
+/*!40000 ALTER TABLE `employes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sqlite_sequence`
+--
+
+DROP TABLE IF EXISTS `sqlite_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sqlite_sequence` (
+  `name` varchar(8) DEFAULT NULL,
+  `seq` tinyint(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sqlite_sequence`
+--
+
+LOCK TABLES `sqlite_sequence` WRITE;
+/*!40000 ALTER TABLE `sqlite_sequence` DISABLE KEYS */;
+INSERT INTO `sqlite_sequence` VALUES ('employes',11);
+/*!40000 ALTER TABLE `sqlite_sequence` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-12-29 11:52:07
